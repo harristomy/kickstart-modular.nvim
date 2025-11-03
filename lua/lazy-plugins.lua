9d88af184e5c4f5247c7f6ecaead9ce1d54154c9
Harris Tomy <harristomy@gmail.com>
2025-11-04 08:01:59 +1100

    update lazy plugins, use default kickstart plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -52,11 +52,11 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -73,19 +73,19 @@ require('lazy').setup({
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
+      --cmd = '⌘',
+      --config = '🛠',
+      --event = '📅',
+      --ft = '📂',
+      --init = '⚙',
+      --keys = '🗝',
+      --plugin = '🔌',
+      --runtime = '💻',
+      --require = '🌙',
+      --source = '📄',
+      --start = '🚀',
+      --task = '📌',
+      --lazy = '💤 ',
     },
   },
 })
